484848c5680e28a185f9a0a308a3fae27e4ded9c
Louis-Maxime Piton <louismaxime.piton@orange.com>
2025-07-16 12:49:33 +0200

    fix(docs): Reintroduce StoryBook

---
+++ storybook/assets/Badge.stories-D3zRXpq8.js
@@ -0,0 +1,239 @@
+const F={title:"Components/Badge",parameters:{docs:{toc:!0}}},e=()=>`<div class="bd-example order-first m-none border-none m-none border-none"> <p class="badge"><span class="visually-hidden">Badge alt text</span></p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`,s=()=>`<div class="bd-example order-first m-none border-none d-flex gap-3 m-none border-none"> <p class="badge"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-accent-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-positive-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-info-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-warning-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-negative-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge badge-disabled"><span class="visually-hidden">Badge alt text</span></p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`,a=()=>`<div class="bd-example order-first m-none border-none d-flex align-items-center gap-3 m-none border-none"> 
+<p class="badge badge-lg"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge badge-sm"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge badge-xs"><span class="visually-hidden">Badge alt text</span></p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`,n=()=>`<div class="bd-example order-first m-none border-none d-flex align-items-center gap-3 m-none border-none"> <p class="badge badge-count">1</p>
+<p class="badge badge-lg badge-count">1</p>
+<p class="badge badge-count text-bg-status-positive-emphasized">23</p>
+<p class="badge badge-count badge-lg text-bg-status-positive-emphasized">23</p>
+<p class="badge badge-count badge-disabled">+99</p>
+<p class="badge badge-count badge-lg badge-disabled">+99</p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`,t=()=>`<div class="bd-example order-first m-none border-none d-flex align-items-center gap-3 m-none border-none"> <p class="badge">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#heart-recommend"></use>
+  </svg>
+  <span class="visually-hidden">Badge alt text</span>
+</p>
+<p class="badge badge-lg">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#heart-recommend"></use>
+  </svg>
+  <span class="visually-hidden">Badge alt text</span>
+</p>
+<p class="badge text-bg-status-negative-emphasized">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#heart-recommend"></use>
+  </svg>
+  <span class="visually-hidden">Badge alt text</span>
+</p>
+<p class="badge badge-lg text-bg-status-negative-emphasized">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#heart-recommend"></use>
+  </svg>
+  <span class="visually-hidden">Badge alt text</span>
+</p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`,d=()=>`<div class="bd-example order-first m-none border-none d-flex align-items-center gap-3 m-none border-none"> <button type="button" class="btn btn-icon btn-default position-relative">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#trash"></use>
+  </svg>
+  <span class="visually-hidden">Trash</span>
+  <span class="position-absolute top-0 start-100 translate-middle badge text-bg-status-negative-emphasized">
+    <span class="visually-hidden">Trash contains items</span>
+  </span>
+</button>
+
+<button type="button" class="btn btn-default position-relative ms-md">
+  Inbox
+  <span class="position-absolute top-0 start-100 translate-middle badge badge-count text-bg-status-info-emphasized">
+    +99
+    <span class="visually-hidden">New mails</span>
+  </span>
+</button> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`,l=()=>`<div class="bd-example order-first m-none border-none m-none border-none"> <button type="button" class="btn btn-primary position-relative">
+Inbox
+<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+  +99
+  <span class="visually-hidden">unread messages</span>
+</span>
+</button> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`,r=()=>`<div class="bd-example order-first m-none border-none m-none border-none"> <button type="button" class="btn btn-primary position-relative">
+  Profile
+  <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+    <span class="visually-hidden">New alerts</span>
+  </span>
+</button> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`,i=()=>`<div class="bd-example order-first m-none border-none m-none border-none"> <span class="badge text-bg-primary">+99</span>
+<span class="badge text-bg-secondary">+99</span>
+<span class="badge text-bg-success">+99</span>
+<span class="badge text-bg-danger">+99</span>
+<span class="badge text-bg-warning">+99</span>
+<span class="badge text-bg-info">+99</span>
+<span class="badge text-bg-light">+99</span>
+<span class="badge text-bg-dark">+99</span> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`,p=()=>`<div class="bd-example order-first m-none border-none m-none border-none"> <span class="badge rounded-pill text-bg-primary">+99</span>
+<span class="badge rounded-pill text-bg-secondary">+99</span>
+<span class="badge rounded-pill text-bg-success">+99</span>
+<span class="badge rounded-pill text-bg-danger">+99</span>
+<span class="badge rounded-pill text-bg-warning">+99</span>
+<span class="badge rounded-pill text-bg-info">+99</span>
+<span class="badge rounded-pill text-bg-light">+99</span>
+<span class="badge rounded-pill text-bg-dark">+99</span> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>`;var o,c,g;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none m-none border-none"> <p class="badge"><span class="visually-hidden">Badge alt text</span></p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(g=(c=e.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};var b,u,v;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none d-flex gap-3 m-none border-none"> <p class="badge"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-accent-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-positive-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-info-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-warning-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge text-bg-status-negative-emphasized"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge badge-disabled"><span class="visually-hidden">Badge alt text</span></p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(v=(u=s.parameters)==null?void 0:u.docs)==null?void 0:v.source}}};var m,x,h;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none d-flex align-items-center gap-3 m-none border-none"> 
+<p class="badge badge-lg"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge badge-sm"><span class="visually-hidden">Badge alt text</span></p>
+<p class="badge badge-xs"><span class="visually-hidden">Badge alt text</span></p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(h=(x=a.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};var f,y,k;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none d-flex align-items-center gap-3 m-none border-none"> <p class="badge badge-count">1</p>
+<p class="badge badge-lg badge-count">1</p>
+<p class="badge badge-count text-bg-status-positive-emphasized">23</p>
+<p class="badge badge-count badge-lg text-bg-status-positive-emphasized">23</p>
+<p class="badge badge-count badge-disabled">+99</p>
+<p class="badge badge-count badge-lg badge-disabled">+99</p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(k=(y=n.parameters)==null?void 0:y.docs)==null?void 0:k.source}}};var B,E,S;t.parameters={...t.parameters,docs:{...(B=t.parameters)==null?void 0:B.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none d-flex align-items-center gap-3 m-none border-none"> <p class="badge">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#heart-recommend"></use>
+  </svg>
+  <span class="visually-hidden">Badge alt text</span>
+</p>
+<p class="badge badge-lg">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#heart-recommend"></use>
+  </svg>
+  <span class="visually-hidden">Badge alt text</span>
+</p>
+<p class="badge text-bg-status-negative-emphasized">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#heart-recommend"></use>
+  </svg>
+  <span class="visually-hidden">Badge alt text</span>
+</p>
+<p class="badge badge-lg text-bg-status-negative-emphasized">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#heart-recommend"></use>
+  </svg>
+  <span class="visually-hidden">Badge alt text</span>
+</p> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(S=(E=t.parameters)==null?void 0:E.docs)==null?void 0:S.source}}};var _,z,j;d.parameters={...d.parameters,docs:{...(_=d.parameters)==null?void 0:_.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none d-flex align-items-center gap-3 m-none border-none"> <button type="button" class="btn btn-icon btn-default position-relative">
+  <svg aria-hidden="true">
+    <use xlink:href="/docs/0.5/assets/img/ouds-web-sprite.svg#trash"></use>
+  </svg>
+  <span class="visually-hidden">Trash</span>
+  <span class="position-absolute top-0 start-100 translate-middle badge text-bg-status-negative-emphasized">
+    <span class="visually-hidden">Trash contains items</span>
+  </span>
+</button>
+
+<button type="button" class="btn btn-default position-relative ms-md">
+  Inbox
+  <span class="position-absolute top-0 start-100 translate-middle badge badge-count text-bg-status-info-emphasized">
+    +99
+    <span class="visually-hidden">New mails</span>
+  </span>
+</button> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(j=(z=d.parameters)==null?void 0:z.docs)==null?void 0:j.source}}};var q,w,A;l.parameters={...l.parameters,docs:{...(q=l.parameters)==null?void 0:q.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none m-none border-none"> <button type="button" class="btn btn-primary position-relative">
+Inbox
+<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+  +99
+  <span class="visually-hidden">unread messages</span>
+</span>
+</button> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(A=(w=l.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};var D,L,W;r.parameters={...r.parameters,docs:{...(D=r.parameters)==null?void 0:D.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none m-none border-none"> <button type="button" class="btn btn-primary position-relative">
+  Profile
+  <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+    <span class="visually-hidden">New alerts</span>
+  </span>
+</button> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(W=(L=r.parameters)==null?void 0:L.docs)==null?void 0:W.source}}};var I,N,T;i.parameters={...i.parameters,docs:{...(I=i.parameters)==null?void 0:I.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none m-none border-none"> <span class="badge text-bg-primary">+99</span>
+<span class="badge text-bg-secondary">+99</span>
+<span class="badge text-bg-success">+99</span>
+<span class="badge text-bg-danger">+99</span>
+<span class="badge text-bg-warning">+99</span>
+<span class="badge text-bg-info">+99</span>
+<span class="badge text-bg-light">+99</span>
+<span class="badge text-bg-dark">+99</span> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(T=(N=i.parameters)==null?void 0:N.docs)==null?void 0:T.source}}};var P,C,O;p.parameters={...p.parameters,docs:{...(P=p.parameters)==null?void 0:P.docs,source:{originalSource:`() => \`<div class="bd-example order-first m-none border-none m-none border-none"> <span class="badge rounded-pill text-bg-primary">+99</span>
+<span class="badge rounded-pill text-bg-secondary">+99</span>
+<span class="badge rounded-pill text-bg-success">+99</span>
+<span class="badge rounded-pill text-bg-danger">+99</span>
+<span class="badge rounded-pill text-bg-warning">+99</span>
+<span class="badge rounded-pill text-bg-info">+99</span>
+<span class="badge rounded-pill text-bg-light">+99</span>
+<span class="badge rounded-pill text-bg-dark">+99</span> </div>
+<script type="text/javascript">
+  /* global oudsWeb: false */
+  document.querySelectorAll('[href]').forEach(link => {link.addEventListener('click', event => {event.preventDefault()})})
+<\/script>\``,...(O=(C=p.parameters)==null?void 0:C.docs)==null?void 0:O.source}}};const G=["Badge_0","Badge_1","Badge_2","Badge_3","Badge_4","Badge_5","Badge_6","Badge_7","Badge_8","Badge_9"];export{e as Badge_0,s as Badge_1,a as Badge_2,n as Badge_3,t as Badge_4,d as Badge_5,l as Badge_6,r as Badge_7,i as Badge_8,p as Badge_9,G as __namedExportsOrder,F as default};
