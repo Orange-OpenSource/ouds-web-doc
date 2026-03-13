c5b009b5335e24184d982419ff339305768aae97
Louis-Maxime Piton <louismaxime.piton@orange.com>
2026-03-13 15:21:53 +0100

    chore(release): v1.1.0 :rocket: (#12)

---
+++ storybook/assets/Chips.stories-C9H-lZaZ.js
@@ -0,0 +1,1 @@
+const t={title:"Components/Chips",parameters:{docs:{toc:!0}}},s=[];export{s as __namedExportsOrder,t as default};
---
+++ storybook/assets/Popover.stories-CWHkIUVM.js
@@ -0,0 +1,1 @@
+const o={title:"Components/Popover",parameters:{docs:{toc:!0}}},e=[];export{e as __namedExportsOrder,o as default};
